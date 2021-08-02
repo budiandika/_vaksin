767f46add61765bc6a113fbc052d9d8d0bc73dd1
budiandika <budiandika33@gmail.com>
2021-08-02 09:59:54 +0700

    up

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -497,19 +497,6 @@
             </div>
         </nav>
     </header>
-    <!--end header-->
-    <!--page-wrapper-->
-    
-    <!--end page-wrapper-->
-    <!--start overlay-->
     <div class="overlay toggle-btn-mobile"></div>
-    <!--end overlay-->
-    <!--Start Back To Top Button--> <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
-    <!--End Back To Top Button-->
-    <!--footer -->
-    <div class="footer">
-        <p class="mb-0">Syndash @2020 | Developed By : <a href="https://themeforest.net/user/codervent" target="_blank">codervent</a>
-        </p>
-    </div>
-    <!-- end footer -->
+        <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
 </div>
